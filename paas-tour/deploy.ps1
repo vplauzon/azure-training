34ad151790825501b72f3b1af6e8f69840a92d18
vplauzon <vincent_lauzon@yahoo.com.au>
2018-07-22 11:19:21 -0400

    .

---
+++ paas-tour/deploy.ps1
@@ -0,0 +1,7 @@
+﻿New-AzureRmResourceGroup -Name paas-tour -Location eastus2 -Force
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName paas-tour `
+                                    -TemplateFile 'paas-tour-arm-template.json' `
+                                    -TemplateParameterFile 'paas-tour-arm-template.parameters.json' `
+                                    -Force -Verbose `
+                                    -ErrorVariable ErrorMessages
